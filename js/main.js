d38d74a6aac5520ebd8f690272eb2aebf387e637
NodileBlog <105820026+NodileBlog@users.noreply.github.com>
2022-05-18 13:53:03 -0600

    First Commit
    demo nodile

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector('.toggle');
+const header= document.querySelector('.header')
+const showcase = document.querySelector('.showcase');
+const icon = document.querySelector('.logo');
+
+
+menuToggle.addEventListener('click', () => {
+  header.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+  icon.classList.toggle('active');
+});
+
+
+
+
